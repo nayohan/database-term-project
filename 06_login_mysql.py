b6da9e8cfbb7c3901d8c18f506c17cdde721ea1a
nudago <lukaslover@naver.com>
2020-11-28 14:09:38 +0900

    [ADD] 회원가입 기능 추가
     회원가입기능으로, 데이터베이스에 id,pw,username,email정보를 insert하고, 완료후 되돌아와서, login페이지에 다시 들어 갈수 있도록함.

--- 06_login_mysql.py
+++ 06_login_mysql.py
@@ -12,18 +12,29 @@ cur = hashtag_db.cursor(pymysql.cursors.DictCursor)
 
 app = Flask(__name__)
 @app.route('/')
-def login():
+def main():
     return render_template('login.html')
 
-@app.route('/user', methods=['POST'])
-def user():
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/register/do', methods=['POST'])
+def do_register():
     _id = request.form['id']
     _pw = request.form['pw']
     _username = request.form['username']
     _email = request.form['email']
     cur.execute('insert into users (id, password,username, email) values (%s, %s, %s, %s)', (_id, _pw, _username, _email))
     hashtag_db.commit()
+    return render_template('register_result.html', id=_id, pw=_pw, username=_username, email=_email)
+
+@app.route('/login', methods=['POST'])
+def login():
+    _id = request.form['id']
+    _pw = request.form['pw']
     return render_template('login_result.html', id=_id, pw=_pw)
+
 if __name__=='__main__':
     app.debug=True
     app.run(host='192.168.0.8', port=5000)
